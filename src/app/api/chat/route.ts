3614580de74927eef970a47e2069b4889d01d8a8
BrayanVargas-HW <brayan.vargas@hatchworks.com>
2024-10-31 17:39:27 -0600

    sistema funcional

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,6 +1,6 @@
 import { BedrockRuntimeClient, ConverseCommand } from '@aws-sdk/client-bedrock-runtime'
 import { marked } from 'marked';
-
+import reference from './reference.json';
 
 const client = new BedrockRuntimeClient({
     region: process.env.AWS_REGION,
@@ -9,17 +9,234 @@ const client = new BedrockRuntimeClient({
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
     },
 })
+const modelId = "us.meta.llama3-2-11b-instruct-v1:0"; // Model ID
 
+let studentProfile: any = {
+    "Language": null,
+    "Name": null,
+    "ScholarYear": null,
+    "Location": null,
+    "Age": null,
+    "Gender": null,
+    "Economy": null,
+    
+    "CognitiveSkills": [],
+    "Interests": [],
+    "SoftSkills": [],
+    "FavoriteSubjects": [],
+    "WorkPreferences": [],
+    "EconomicConstraints": [],
+    "LearningStyle": [],
+    "TechnologicalAffinity": []
+};
+let questionsMade: string[] = [];
+let recommendationGiven: boolean = false;
+
+function missingAspects() {
+    let aspectKey = null;
+    for (const [key, value] of Object.entries(studentProfile)) {
+        if (Array.isArray(value) && value.length < 2) {
+            aspectKey = key;
+            break;
+        }
+        if (value === null || value === undefined || value === '') {
+            aspectKey = key;
+            break;
+        }
+    }
+    return aspectKey;
+}
+
+async function basicProfileEvaluation(userText: string, missingAspect: string) {
+    const instructions: any[] = [{
+        role: "user",
+        content: [{'text':
+            "You are a text analyzer. You are given a system question and a user answer, and you need to analyze"+
+            " the text and extract from the user answer the information that fits to the following aspects \n"+
+            JSON.stringify(studentProfile)+
+            "\n For this extraction try to focus on the information related to: "+missingAspect+
+            "\n\n Here you have the information to analyze \n"+userText+
+            "\n\n For your response, reply with a json format text copying the aspects presented before, keep the"+
+            " values that are already established and append to the lists the new values recognized in the user response"+
+            "\n the values for name, location, language, gender, age and economy are strings, update them if the user answer give a new value"+
+            "\n Give me just the json profile with the new values. You are forbidden to add any details, comments, or markdown formatting"}]
+    
+            // },{
+    //     role: "assistant",
+    //     content: [{"text":"Great, please provide the text you want me to analyze."}]
+    // },{
+    //     role: "user",
+    //     content: [{"text":"Here is the text: " + userText + 
+    //     "\n\n First, if this Q/A fits to one or more of the following profile aspects, please provide the new profile status by updating the values. Add the value just if the user answer give a positive answer."+
+    //     "and try to focus on the aspect: "+missingAspect+
+    //     JSON.stringify(studentProfile)+
+    //     "\nDon't remove the values that are already present, just update the ones that are changed. In case of the lists, append the values to the existing ones."+
+    //     "\nGive me just the json profile with the new values. You are forbidden to add any details or comments, I need just the values in the json object"}]
+    }]
+    const command = new ConverseCommand({
+        modelId,
+        messages: instructions,
+        inferenceConfig: { maxTokens: 512, temperature: 0.5, topP: 0.9 },
+    });
+
+    let response = await client.send(command);
+    console.log("Model response success");
+    let assistantMessage = response.output?.message;
+    if (!assistantMessage) {
+        return null;
+    }
+    let rawText = assistantMessage.content?.[0].text ?? '';
+    rawText = rawText.replaceAll("'", '"');
+    while ((rawText.match(/{/g) || []).length > 1 && (rawText.match(/}/g) || []).length > 1) {
+        console.log("Re evaluating the profile");
+        response = await client.send(command);
+        assistantMessage = response.output?.message;
+        if (!assistantMessage) {
+            return null;
+        }
+        rawText = assistantMessage.content?.[0].text ?? '';
+        rawText = rawText.replaceAll("'", '"');
+    }
+    rawText = rawText.substring(rawText.indexOf("{"), rawText.lastIndexOf("}") + 1);
+    if (assistantMessage.content && assistantMessage.content[0]) {
+        assistantMessage.content[0].text = rawText;
+    }
+    instructions.push(assistantMessage);
+    console.log("Raw text: ", rawText);
+    const evaluation = JSON.parse(rawText);
+
+    for (const aspect in evaluation) {
+        if (evaluation[aspect] != null) {
+            studentProfile[aspect] = evaluation[aspect];
+        }
+    }
+    const missing = missingAspects();
+    if(missing == null){
+        return {
+            "evaluation": evaluation,
+            "question": "I have no more questions for you, let's start with the career guidance."
+        };
+    }
+    instructions.push({
+        role: "user",
+        content: [{"text":"Perfect, now for getting more info, I need you to take a question for "+
+            "one of the aspects that are lacking in the evaluation. I just need the question, don't add details."+
+            "\nIn this case, the aspect that is lacking is: "+missing+
+            "\nHere you have the reference of the aspects, take one of the possible questions: "+JSON.stringify(reference[missing as keyof typeof reference])+
+            "\nAnd don't repeat this questions: " + questionsMade.join("\n -")
+        }]
+    });
+    const suggestionsResponse = await client.send(command);
+    const question = suggestionsResponse.output?.message?.content?.[0].text ?? '';
+    console.log("Missing aspect: ", missing, " - question: ", question);
+    console.log("Questions reference: ", reference[missing as keyof typeof reference]);
+    questionsMade.push(question);
+    const evaluationResult = {
+        "evaluation": evaluation,
+        "question": question
+    };
+    return evaluationResult;
+}
+
+async function createProfileAspects() {
+    let AspectsScores: any = {};
+    for (const area in reference) {
+        AspectsScores[area] = Object.keys(reference[area as keyof typeof reference]);
+    }
+    const instructions: any[] = [{
+        role: "user",
+        content: [{"text":"You are a psychologist that can help with vocational guidance. You are given with a student profile, "+
+            "and you need to provide a effective evaluation of different aspects that I will give you. "+
+            "\nHere you have the profile: "+JSON.stringify(studentProfile)+
+            "\nAnd the aspects that you need to evaluate: "+JSON.stringify(AspectsScores)+
+            "\n\nFor your evaluation, take each aspect and score it from 0 to 10, based on the profile of the student. "+
+            "\nGive me just a json object with the evaluation of the aspects. You are forbidden to add any details or comments, I need just the values in the json object"}]
+    }]
+    const command = new ConverseCommand({
+        modelId,
+        messages: instructions,
+        inferenceConfig: { maxTokens: 1024, temperature: 0.5, topP: 0.9 },
+    });
+    const response = await client.send(command);
+    let rawText = response.output?.message?.content?.[0].text ?? '';
+    rawText = rawText.substring(rawText.indexOf("{"), rawText.lastIndexOf("}") + 1);
+    const aspectsEvaluation = JSON.parse(rawText);
+    
+    return aspectsEvaluation;
+}
 // Define the conversation handler for Bedrock's "converse" functionality
-async function handleConversation(messages: any) {
-    const modelId = "us.meta.llama3-2-11b-instruct-v1:0"; // Model ID
+async function handleConversation(messages: any, question: string) {
+    
+    // Add initial context message to guide the LLM's behavior
+    const systemContext = {
+        role: "user",
+        content: [{ 'text': "You are an AI assistant specialized in vocational guidance for students. Your goal is to help students explore their interests, skills, and career aspirations through conversation."+
+            "\n here you have the current profile of the student: "+JSON.stringify(studentProfile)
+        }]
+    };
 
     const conversation = messages.map((message: any) => ({
         role: message.role,
         content: [{ 'text': message.content }]
     }));
 
+    const userAnswer = conversation.slice(-1)[0].content[0].text;
+    conversation[conversation.length - 1].content = [{'text': "User answer: " + userAnswer+
+        "\n\nAnswer to the user in a brief tone, and you can continue with the question: '" + question+"'.\n"+
+        "If the question is personal, be careful with the tone and try to avoid being too intrusive. Example: “I'd also like to know, how do you identify in terms of gender? Feel free to share however you're comfortable!”"+
+        "Remember you are talking with the user, so act naturally and answer to the user with the provided question. Be as short and concise as possible."
+    }];
     // Send the request to the Bedrock model
+    const command = new ConverseCommand({
+        modelId,
+        messages: [systemContext, ...conversation],
+        inferenceConfig: { maxTokens: 512, temperature: 0.5, topP: 0.9 },
+    });
+    const response = await client.send(command);
+    const rawText = response.output?.message?.content?.[0].text ?? '';
+    const message = marked.parse(rawText); // Converts MD to HTML
+    return message; // Return the properly formatted message
+}
+
+async function suggestCareers(messages: any[], aspectScores: any) {
+    const conversation = messages.map((message: any) => ({
+        role: message.role,
+        content: [{ 'text': message.content }]
+    }));
+    const instructions :any[] = [
+        {
+            role: "assistant",
+            content: [{ 'text': "Ok, based in all the information you have, here you have a summary of the evaluation of the aspects: "+
+                JSON.stringify(aspectScores)+
+                "Does this summary fit to your profile?"
+            }]
+        },{
+        role: "user",
+        content: [{ 'text': 
+            "Yes, it's perfects! Now based in that summary, provide a brief summary of the evaluation and suggest 3-5 potential career paths that would be a good match." +
+            "\nFocus on careers that align with my strongest aspects." +
+            "\nKeep the response concise but informative, using bullet points for the career suggestions." +
+            "\nFormat the response in markdown. Give your answer in "+studentProfile.Language
+        }]
+    }];
+
+    const command = new ConverseCommand({
+        modelId,
+        messages: [...conversation, ...instructions],
+        inferenceConfig: { maxTokens: 1024, temperature: 0.7, topP: 0.9 },
+    });
+
+    const response = await client.send(command);
+    const rawText = response.output?.message?.content?.[0].text ?? '';
+    return marked.parse(rawText); // Convert markdown to HTML
+}
+
+async function chat(messages: any[]) {
+    const conversation = messages.map((message: any) => ({
+        role: message.role,
+        content: [{ 'text': message.content }]
+    }));
+    conversation[conversation.length - 1].content[0].text += "\n\n Give your answer in "+studentProfile.Language+" and be concise, format your answer with html tags.";
     const command = new ConverseCommand({
         modelId,
         messages: conversation,
@@ -27,27 +244,49 @@ async function handleConversation(messages: any) {
     });
     const response = await client.send(command);
     const rawText = response.output?.message?.content?.[0].text ?? '';
-    const responseText = marked.parse(rawText); // Converts MD to HTML
-
-    const message = {
-        id: crypto.randomUUID(), // or any unique ID generation method
-        role: 'assistant',
-        content: responseText
-    };
-
-    return message; // Return the properly formatted message
+    const message = marked.parse(rawText); // Converts MD to HTML
+    return message;
 }
 
 export async function POST(req: Request) {
-    console.log("Received request");
+    const message = {
+        id: crypto.randomUUID(), // or any unique ID generation method
+        role: 'assistant',
+        content: ''
+    };
     try {
         const { messages } = await req.json();
-        console.log(messages);
-        const response = await handleConversation(messages);
+        const lastTwoMessages = messages.slice(-2);
+        
+        const userQA = `Question: ${lastTwoMessages[0].content}\n >>User Answer: ${lastTwoMessages[1].content}`;
+        const missing = missingAspects();
+        console.log("Missing aspect: ", missing);
+        if(missing == null) {
+            if(!recommendationGiven){   
+                // evaluation of career traits
+                const aspectsEvaluation = await createProfileAspects();
+                console.log("Aspects evaluation: ", aspectsEvaluation);
 
-        return new Response(JSON.stringify(response));
+                message.content = await suggestCareers(messages.slice(-7), aspectsEvaluation);  
+                recommendationGiven = true;
+            }else{
+                message.content = await chat(messages.slice(-11));
+            }
+        }else{
+            // ask for basic information
+            const evaluation = await basicProfileEvaluation(userQA, missing);
+            if (!evaluation) {
+                message.content = "Sorry, I didn't understand your response, can you give me more details.";
+                return new Response(JSON.stringify({"message": message, "evaluation": null, "profile": studentProfile}), { status: 200 });
+            }
+            message.content = await handleConversation(messages.slice(-4), evaluation['question']);
+        }
+        
+        
+        return new Response(JSON.stringify({"message": message, "evaluation": null, "profile": studentProfile}));
     } catch (error) {
+        message.content = 'An error occurred while processing your request. Please try again later.';
         console.error("Error during chat invocation:", error);
-        return new Response(JSON.stringify({ error: "Failed to invoke Bedrock model" }), { status: 500 });
+        return new Response(JSON.stringify({"message": message, "evaluation": null, "profile": studentProfile}), { status: 500 });
     }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,32 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
+import { RiSendPlaneFill } from "react-icons/ri"
 
 export default function Chat() {
   // Load initial messages from localStorage
-  const initialMessages = typeof window !== 'undefined' 
+  const initialMessages = typeof window !== 'undefined'
     ? JSON.parse(localStorage.getItem('chatMessages') || '[]')
     : [];
-
+  const specificDivRef = useRef<HTMLDivElement>(null);
   const [messages, setMessages] = useState(initialMessages);
   const [input, setInput] = useState('');
-  // const { messages, input, handleInputChange, handleSubmit } = useChat({
-  //   api: '/api/chat',
-  //   initialMessages,
-  // })
-
-//  useEffect(() => {
-//    localStorage.setItem('chatMessages', JSON.stringify(messages));
-//  }, [messages]);
+  const [studentProfile, setStudentProfile] = useState(null);
+  
+  useEffect(() => {
+    setMessages([{
+      id: crypto.randomUUID(),
+      role: 'assistant', 
+      content: '<p>Hello, I am a chatbot that can help you with vocational guidance. I hope to help you find your path.</p> <p>First,what is your name? Then, tell me if you are comfortable speaking english or would you prefer another language.</p>'
+    }]);
+  }, []);
+  
+  useEffect(() => {
+    if (specificDivRef.current) {
+      specificDivRef.current.scrollTop = specificDivRef.current.scrollHeight;
+    }
+  });
 
   const [isTyping, setIsTyping] = useState(false)
 
@@ -28,78 +36,106 @@ export default function Chat() {
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (input.trim() === '') return;
 
     setIsTyping(true)
     let messagesCopy = [...messages];
-    messagesCopy.push({id: crypto.randomUUID(), role: "user", content: input});
+    messagesCopy.push({ id: crypto.randomUUID(), role: "user", content: `<p>${input}</p>`});
     setMessages(messagesCopy);
     setInput('');
 
     const response = await fetch('/api/chat', {
       method: 'POST',
-      body: JSON.stringify({messages: messagesCopy})
+      body: JSON.stringify({ messages: messagesCopy })
     })
     const aiMessage = await response.json();
     console.log(aiMessage);
-    messagesCopy.push(aiMessage);
+    messagesCopy.push(aiMessage.message);
+    setStudentProfile(aiMessage.profile);
     setMessages(messagesCopy);
     setIsTyping(false)
   }
 
   return (
-    <div className='mx-auto w-full max-w-2xl'>
-      <div className="flex flex-col h-screen">
-        <div className="flex-1 overflow-y-auto p-4">
-        <AnimatePresence>
-          {messages.map((message: any) => (
-            <motion.div
-              key={message.id}
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -20 }}
-              className={`mb-4 ${
-                message.role === 'user' ? 'text-right' : 'text-left'
-              }`}
-            >
-              <div
-                className={`inline-block p-2 rounded-lg message ${
-                  message.role === 'user'
-                    ? 'bg-cyan-700 text-white'
-                    : 'bg-gray-800 text-gray-100'
-                }`}
-                dangerouslySetInnerHTML={{ __html: message.content }}
-              />
-            </motion.div>
-          ))}
-        </AnimatePresence>
-        {isTyping && (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            className="text-gray-100 text-sm"
-          >
-            AI is typing...
-          </motion.div>
-        )}
-      </div>
-      <form onSubmit={onSubmit} className="p-4">
-        <div className="flex">
-          <input
-            type="text"
-            value={input}
-            onChange={handleInputChange}
-            placeholder="Type your message..."
-            className="flex-1 p-2 me-4 border rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-600 bg-gray-800"
-          />
-          <button
-            type="submit"
-            className="bg-cyan-700 text-white p-2 rounded-lg hover:bg-cyan-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          >
-            Send
-          </button>
+
+    <div className="flex gap-2">
+      {studentProfile && (
+        <div className='w-[40%] flex flex-col h-screen'>
+          <h2 className='text-white text-xl font-bold mb-4'>Profile Evaluation</h2>
+          <div className='flex-1 overflow-y-auto custom-scrollbar'>
+            <pre>{JSON.stringify(studentProfile, null, 2)}</pre>
+          </div>
+          {/* <div className='flex flex-wrap'>
+            {Object.keys(AspectsScores).map((aspect) => (
+              <div key={aspect} className={`w-48 mx-auto my-1 flex justify-between p-2 rounded-md ${colors[Math.floor(AspectsScores[aspect])]}`}>
+              <h3 className='text-white text-sm'>{aspect}</h3>
+              <p className='text-white text-sm'>{Number(AspectsScores[aspect]).toFixed(2)}</p>
+              </div>
+            ))}
+          </div> */}
         </div>
-      </form>
+      )}
+      <div className='mx-auto w-full'>
+        <div className="flex flex-col h-screen">
+          <div className="flex-1 overflow-y-auto p-4 custom-scrollbar" ref={specificDivRef}>
+            <div className="max-w-2xl mx-auto">
+              <AnimatePresence>
+                {messages.map((message: any) => (
+                  <motion.div
+                    key={message.id}
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -20 }}
+                    className={`mb-4 text-left flex ${message.role === 'user' ? 'flex-row-reverse' : ''}`}
+                  >
+                    <div
+                      className={`inline-block p-2 rounded-lg message ${message.role === 'user'
+                        ? 'bg-cyan-700 text-white'
+                        : 'bg-gray-800 text-gray-100'
+                        }`}
+                      dangerouslySetInnerHTML={{ __html: message.content }}
+                    />
+                  </motion.div>
+                ))}
+              </AnimatePresence>
+              {isTyping && (
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  className="text-gray-100 text-sm"
+                >
+                  AI is typing...
+                </motion.div>
+              )}
+            </div>
+
+          </div>
+          <div className='w-full flex justify-center'>
+            <form onSubmit={onSubmit} className="flex p-1 ps-3 rounded-[2rem] mb-8 bg-gray-700 w-[95%] sm:w-[90%] md:w-[80%] lg:w-[70%] xl:w-[60%]">
+              <div className="flex w-full h-full">
+                <div className='flex-1 p-2 me-4 h-fit'>
+                  <input
+                    value={input}
+                    onInput={handleInputChange}
+                    placeholder="Type your message..."
+                    className="w-full focus:outline-none bg-transparent resize-none text-white"
+                  />
+                </div>
+
+                <button
+                  type="submit"
+                  className={`bg-cyan-700 text-white p-1 m-1 rounded-full hover:bg-cyan-500 focus:outline-none focus:ring-2 focus:ring-blue-500 w-8 h-8 text-center
+              ${input.trim() === '' ? 'opacity-50 cursor-not-allowed' : ''}`}
+                >
+                  <RiSendPlaneFill className='text-xl' />
+                </button>
+              </div>
+            </form>
+          </div>
+
+        </div>
+      </div>
     </div>
-    </div>
+
   )
 }
